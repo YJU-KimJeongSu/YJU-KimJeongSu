ce90d513a3e4d03f68710ae6d3eca08d34a9f225
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2022-06-27 10:31:56 +0900

    update
    2751 완성 못함 (타임오버)

---
+++ GitUp/src/Baekjoon/KJS_Q1085.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class KJS_Q1085 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		
+		// x, y, w-x, h-y
+		if (x <= y && x <= w-x && x <= h-y) { System.out.println(x); }
+		else if (y <= x && y <= w-x && y <= h-y) { System.out.println(y); }
+		else if (w-x <= x && w-x <= y && w-x <= h-y) { System.out.println(w-x); }
+		else { System.out.println(h-y); }
+	}
+
+}
---
+++ GitUp/src/Baekjoon/KJS_Q2751.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class KJS_Q2751 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] x = new int[n];
+		
+		for (int i = 0; i < x.length; i++) {
+			x[i] = sc.nextInt();
+		}
+		
+		bubbleSort(x);
+		
+		for (int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length-1-i; j++) {
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+}
