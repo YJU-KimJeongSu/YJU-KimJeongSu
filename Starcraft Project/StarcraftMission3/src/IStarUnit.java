8a26877e706ebe6f50f2d57233e8d2ddb7563aa8
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2022-07-01 09:35:36 +0900

    Mission 1~3
    1. 로딩창만 뜨게
    2. 커맨드랑 SCV만 남기기
    3. 스레드 적용, SCV 각자 상태 표시

---
+++ Starcraft Project/StarcraftMission3/src/IStarUnit.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.ArrayList;
+
+interface IStarUnit {
+	public String getTribe(); //종족정보
+	public String getName(); //유닛이름정보
+	public int getEnergy(); //유닛 에너지 정보
+	public int getPower(); //유닛 파워정보
+	public void progressing(int time); 
+}
